7ac6c43b0389790f58f70b4636589cc1757828fe
pooja <pooja@gamil.com>
2025-01-10 14:02:11 +0530

    index fix

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,7 @@ require("dotenv").config()
 
 const app = express()
 app.use(express.json())
-app.use(cors({ origin: "http://localhost:5173", credentials: true }))
+app.use(cors({ origin: true, credentials: true }))
 app.use('/api/v1/todos', todoRoutes);
 
 app.use("*", (req: Request, res: Response) => {
